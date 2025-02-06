943cc14caf64175dd6bdca9764833a0c919be1f5
ZapcoMan <3507557934@qq.com>
2025-02-06 18:50:03 +0800

    用户对象 添加注释

--- src/model/User.ts
+++ src/model/User.ts
@@ -4,16 +4,28 @@
  * Description:
  * Others:
  */
+/**
+ * User类代表一个用户对象，包含了用户的用户名和密码信息
+ */
 export default class User{
     username:string;
     password:string;
+
+    /**
+     * 构造一个新的User实例
+     * @param username 用户名
+     * @param password 密码
+     */
     constructor(username:string,password:string) {
         this.username = username;
         this.password = password;
     }
+
+    /**
+     * 打印用户信息，包括用户名和密码
+     */
     toString(){
         console.log("用户名:" + this.username + "密码:" + this.password);
     }
 }
-const user = new User("codervibe","123456");
-user.toString()
+
