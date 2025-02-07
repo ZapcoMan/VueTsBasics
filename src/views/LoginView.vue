8792ca55d8d50bafefa579f2a322797a0cc70bc1
ZapcoMan <3507557934@qq.com>
2025-02-07 13:45:37 +0800

    更新登录页面

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,17 +1,11 @@
 <script setup lang="ts">
-// 移除 export default defineComponent
-import { ref } from 'vue';
 
-
-// 如果需要返回值，可以直接定义响应式数据或方法
-const message = ref('Welcome to the Login View');
 </script>
 
 <template>
-  <!-- 使用模板语法 -->
-  <div>{{ message }}</div>
+
 </template>
 
 <style scoped>
-/* 样式保持不变 */
+
 </style>
